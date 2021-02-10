147deda1491c177e837d74b9b5462a1a56ed1e3d
srmcfly <d.alejandro198@gmail.com>
2021-02-10 16:55:39 -0500

    1.12

--- main.cpp
+++ main.cpp
@@ -21,5 +21,6 @@ int main()
     std::cout <<"⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢸⣿⣿⣿⠸⣿⡇⣿⣿⡆⣼⣿⣿⣿⡇⣿⣿⡇"<< std::endl;
     std::cout <<"⠇⢀⠄⠄⠄⠄⠄⠘⣿⣿⡘⣿⣿⣷⢀⣿⣷⣿⣿⡿⠿⢿⣿⣿⡇⣩⣿⡇"<< std::endl;
     std::cout <<"⣿⣿⠃⠄⠄⠄⠄⠄⠄⢻⣷⠙⠛⠋⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⡇⣿⣿⡇"<< std::endl;
+    std::cout <<"david es una perra"<< std::endl;
     return 0;
 }
