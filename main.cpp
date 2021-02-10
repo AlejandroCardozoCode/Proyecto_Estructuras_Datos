41c2172dfda2df1ed714445032bdf5a9b5bb16bd
srmcfly <d.alejandro198@gmail.com>
2021-02-10 15:07:19 -0500

    1.0

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// proyecto de estructuras 
+
+#include <iostream>
+
+int main()
+{
+    std::string variable;
+
+    variable = "hola mundo";
+
+    std::cout<<variable<<std::endl;
+    return 0;
+}
